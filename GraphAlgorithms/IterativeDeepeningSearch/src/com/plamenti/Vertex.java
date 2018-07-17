a560e55db940190972078f7f9cedb8c97c80e506
plamenti <plamenti_@abv.bg>
2018-07-17 23:18:38 +0300

    Add Vertex in IDDFS project

---
+++ GraphAlgorithms/IterativeDeepeningSearch/src/com/plamenti/Vertex.java
@@ -0,0 +1,41 @@
+package com.plamenti;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vertex{
+    private String name;
+    private int depthLevel;
+    List<Vertex> neighbors;
+
+    public Vertex(String name){
+        this.name = name;
+        this.depthLevel = 0;
+        this.neighbors = new ArrayList<Vertex>();
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getDepthLevel(){
+        return depthLevel;
+    }
+
+    public void setDepthLevel(int depthLevel){
+        this.depthLevel = depthLevel;
+    }
+
+    public List<Vertex> getNeighbors(){
+        return new ArrayList<Vertex>(neighbors);
+    }
+
+    public void addNeighbor(Vertex vertex){
+        this.neighbors.add(vertex);
+    }
+
+    @Override
+    public String toString(){
+        return this.name;
+    }
+}
